6a137a453ffa107fea80f97d2d5b487b2c050f89
Kevin Song <kevin.song@scotiabank.com>
2019-03-21 20:14:33 -0400

    fix issue in previewing

--- src/generate_tfrecord.py
+++ src/generate_tfrecord.py
@@ -27,7 +27,7 @@ import io
 import pandas as pd
 import tensorflow as tf
 import matplotlib.pyplot as plt
-from matplotlib import patches
+from matplotlib import *
 from matplotlib.patches import Patch
 from utils import *
 
